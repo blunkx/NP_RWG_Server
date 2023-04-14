8c91e2e8d8144e9b1c5b389493e9e38280a4349f
Kuan-Lin Chen <asd9521889@gmail.com>
2023-04-14 16:09:08 +0800

    add test code for print tab

---
+++ test/test_print_tab.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "14"
+         << "\t"
+         << "name"
+         << "\t"
+         << "192.168.100.1:20"
+         << "\t"
+         << "<-me";
+    cout << endl;
+}
