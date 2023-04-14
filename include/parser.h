6833328992a4895a603930ce62f68a65be4fb647
Kuan-Lin Chen <asd9521889@gmail.com>
2023-04-14 16:04:41 +0800

    add more enum type of command

--- include/parser.h
+++ include/parser.h
@@ -22,8 +22,13 @@ typedef enum BUILT_IN_COM_T
     SETENV,
     PRINTENV,
     EXIT,
+    WHO,
+    TELL,
+    YELL,
+    NAME,
 } BUILT_IN_COM_E;
 
 void parser(std::string &input, std::vector<command> &number_pipes);
+void parser(std::string &input, std::vector<user_info> &user_info_arr, size_t id);
 
 #endif
