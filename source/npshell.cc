7fcdca63b21f8db0ac21b5bedf7973229e86725e
blunkx <asd9521889@gmail.com>
2023-04-27 15:21:07 +0800

    add non block io

--- source/npshell.cc
+++ source/npshell.cc
@@ -18,3 +18,35 @@ void exe_shell()
         }
     }
 }
+
+void exe_shell(user_info_shm_ver *user_info_arr, size_t id)
+{
+
+    std::vector<command> cmds;
+    fd_set all_fds;
+    FD_ZERO(&all_fds);
+    FD_SET(STDIN_FILENO, &all_fds);
+    fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
+    std::cout << "% " << std::flush;
+    while (true)
+    {
+        if (strlen(user_info_arr[id].broadcast_msg) != 0)
+        {
+            std::cout << user_info_arr[id].broadcast_msg << std::flush;
+            memset(user_info_arr[id].broadcast_msg, 0, sizeof(user_info_arr[id].broadcast_msg));
+        }
+        char buffer[15001] = {0};
+        ssize_t read_len = read(STDIN_FILENO, buffer, sizeof(buffer));
+        std::string input(buffer);
+        if (read_len > 0)
+        {
+            parser(input, cmds, user_info_arr, id);
+            if (strlen(user_info_arr[id].broadcast_msg) != 0)
+            {
+                std::cout << user_info_arr[id].broadcast_msg << std::flush;
+                memset(user_info_arr[id].broadcast_msg, 0, sizeof(user_info_arr[id].broadcast_msg));
+            }
+            std::cout << "% " << std::flush;
+        }
+    }
+}
